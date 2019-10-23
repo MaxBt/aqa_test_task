cfbf7ab8cdd97b2ae089c5707695bb89407d38e8
maxim <6tstiHLL6six>
2019-10-23 14:30:17 +0300

    make tests parametrized

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,33 +1,22 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 class CashbackHackServiceTest {
 
-    private CashbackHackService cashbackHackService;
+    private CashbackHackService cashbackHackService =  new CashbackHackService();
 
-    @BeforeEach
-    void setUp() {
-        //given
-        cashbackHackService = new CashbackHackService();
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void test_should_not_suggest_when_sum_aliquot_to_1000() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/cachbackedAmounts.csv", numLinesToSkip = 1)
+    void test_should_suggest_exact_value(int sum, int expectedUpsell) {
         //when
-        int upsellAmount = cashbackHackService.remain(3000);
+        int actualUpsell = cashbackHackService.remain(sum);
 
         //then
-        Assertions.assertEquals(0, upsellAmount);
+        Assertions.assertEquals(expectedUpsell, actualUpsell);
     }
 
     @Test
@@ -39,29 +28,10 @@ class CashbackHackServiceTest {
     }
 
     @Test
-    void test_should_suggest_minimal_amount() {
-        //when
-        int upsellAmount = cashbackHackService.remain(999);
-
-        //then
-        Assertions.assertEquals(1, upsellAmount);
-    }
-
-    @Test
     void test_should_not_suggest_when_amount_is_negative() {
         //when //then
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             cashbackHackService.remain(-1500);
         });
     }
-
-    @Test
-    void test_should_suggest_maximal_amount() {
-        //when
-        int upsellAmount = cashbackHackService.remain(Integer.MAX_VALUE);
-
-        //then
-        Assertions.assertEquals(353, upsellAmount);
-    }
-
 }
